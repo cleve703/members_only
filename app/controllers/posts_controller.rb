1bbfff19b442e92e7be7cdc5865b211ffbfcf326
jay <cleve703@gmail.com>
2020-05-01 20:00:44 -0400

    add users controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,15 +7,27 @@ class PostsController < ApplicationController
 
   def new
     @post = Post.new
-    if @post.save
-      reroute to: 'root'
-    else
-      reroute to: 'root'
-    end
   end
 
   def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      flash[:success] = "Post created!"
+      redirect_to root_url
+    else
+      render 'posts/new'
+    end
   end
 
+    private
+
+    def post_params
+      params.require(:post).permit(:content)
+    end
+
+    def correct_user
+      @post = current_user.posts.find_by(id: params[:id])
+      redirect_to root_url if @post.nil?
+    end
 
 end
